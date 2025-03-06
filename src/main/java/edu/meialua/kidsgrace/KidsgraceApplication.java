7492f6e0ffd9a3e79abd130dd7fc29fc7b722ba7
Bruno <67852736+AnsiredDS@users.noreply.github.com>
2025-03-06 17:35:27 -0300

    Add files via upload
    First commit

---
+++ src/main/java/edu/meialua/kidsgrace/KidsgraceApplication.java
@@ -0,0 +1,13 @@
+package edu.meialua.kidsgrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsgraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsgraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/meialua/kidsgrace/KidsgraceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.meialua.kidsgrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsgraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
